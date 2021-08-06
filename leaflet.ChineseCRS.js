dce9d2350d47fb26ca663b5e4382bdae738d78fa
Dingyuan Wang <gumblex@aosc.io>
2021-08-06 13:31:30 +0800

    support AMD/CommonJS

--- leaflet.ChineseCRS.js
+++ leaflet.ChineseCRS.js
@@ -1,4 +1,19 @@
-(function(L) {
+(function (factory, window) {
+
+    // define an AMD module that relies on 'leaflet'
+    if (typeof define === 'function' && define.amd) {
+        define(['leaflet'], factory);
+
+    // define a Common JS module that relies on 'leaflet'
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('leaflet'));
+    }
+
+    // attach your plugin to the global 'L' variable
+    if (typeof window !== 'undefined' && window.L) {
+        window.L.ChineseCRS = factory(L);
+    }
+}(function(L) {
 'use strict';
 
 function _coord_diff(a, b) {
@@ -282,7 +297,7 @@ function getTileLayer(type, url, options) {
 }
 
 
-L.ChineseCRS = {
+var ChineseCRS = {
     wgs_gcj: wgs_gcj,
     wgs_bd: wgs_bd,
     gcj_wgs: gcj_wgs_exact,
@@ -297,4 +312,6 @@ L.ChineseCRS = {
     getTileLayer: getTileLayer,
 };
 
-})(L);
+return ChineseCRS;
+
+}, window));
